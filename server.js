14e1ea0640ceabb89a64ad4e7355fde9f00cfcd3
sournachos <joserivera18100@gmail.com>
2021-05-14 17:11:52 -0500

    test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
@@ -12,10 +13,16 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public" )))
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("../routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
